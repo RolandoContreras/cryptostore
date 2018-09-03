eade510d5d4f9b5cbbad47c52911f3f0f3be13df
unknown <RolandoContreras>
2018-09-03 16:00:02 -0500

    create view faq dinamic home

--- application/controllers/faq.php
+++ application/controllers/faq.php
@@ -4,12 +4,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Faq extends CI_Controller {
     public function __construct(){
      parent::__construct();
-     
+        $this->load->model("dinamic_model","obj_dinamic");
     } 
 
     public function index()
 	{
+        //GET DATA FAQ
+        $params = array(
+                        "select" =>"dinamic_id,
+                                    date,
+                                    title,
+                                    content,
+                                    active,",
+                         "where" => "faq = 1 and active = 1 and status_value = 1",
+            );
+            //GET DATA COMMENTS
+            $obj_faq= $this->obj_dinamic->search($params);
+            $data['obj_faq'] = $obj_faq;
         //RENDER
-        $this->load->view('faq');
+        $this->load->view('faq',$data);
 	}
 }
